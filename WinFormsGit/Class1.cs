b06b09edbe541bc317b499022e72ab7b7f752c5e
Minh Tin Trần <minhtin.spk@gmail.com>
2024-09-10 14:19:59 +0700

    Modify Class 1

---
+++ WinFormsGit/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsGit
+{
+    internal class Class1
+    {
+        public int ID {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
